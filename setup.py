9a9691fb79e1053fc3acd333abd8af7ecc0b50a2
Tyler Bell <tyler@tracetechnology.com>
2018-07-23 14:23:57 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name="srec2bin_py",
+      description="This utility converts binary data from Motorola S-Record file format to raw binary",
+      version="1.0",
+      py_modules=["srec2bin",
+                  "srecord"])
